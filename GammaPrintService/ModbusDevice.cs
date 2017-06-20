3845db61b61d6a46bcc2f07d1dcf363b3cae502d
Matt <mv.polidanov@gmail.com>
2017-06-21 00:08:21 +0300

    Added some base files

---
+++ GammaPrintService/ModbusDevice.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Advantech.Adam;
+using GammaService.Common;
+
+namespace GammaPrintService
+{
+    public class ModbusDevice
+    {
+        public ModbusDevice(DeviceType deviceType, string ipAddress, int placeId, string printerName, int signalChannelNumber, int? confirmChannelNumber, int timerTickTime)
+        {
+            IpAddress = ipAddress;
+            DeviceType = deviceType;
+            PrinterName = printerName;
+            PlaceId = placeId;
+            SignalChannelNumber = signalChannelNumber;
+            ConfirmChannelNumber = confirmChannelNumber;
+            InitializeDevice(deviceType, ipAddress);
+            MainTimer = new Timer(ReadCoil, null, 0, timerTickTime);
+        }
+
+        private string IpAddress { get; set; }
+        private DeviceType DeviceType { get; set; }
+
+        public bool IsConnected { get; private set; }
+
+        public string PrinterName { get; set; }
+
+        private int PlaceId { get; set; }
+
+        private AdamSocket AdamModbus { get; set; }
+
+        private int m_iDiTotal;
+        private int m_iDoTotal;
+
+        private int SignalChannelNumber { get; set; }
+        private int? ConfirmChannelNumber { get; set; }
+
+        private Timer MainTimer { get; }
+        private Timer RestoreConnectTimer { get; set; }
+
+        private void RestoreConnect(object obj)
+        {
+            if (!AdamModbus.Connected)
+            {
+                ReinitializeDevice();
+                if (!AdamModbus.Connected) return;
+                IsConnected = true;
+                Console.WriteLine(DateTime.Now + ": Связь с " + PrinterName + " восстановлена");
+                RestoreConnectTimer?.Dispose();
+                RestoreConnectTimer = null;
+            }
+            else
+            {
+                RestoreConnectTimer?.Dispose();
+                RestoreConnectTimer = null;
+            }
+        }
+
+        private void ReadCoil(object obj)
+        {
+            if (!AdamModbus.Connected)
+            {
+                IsConnected = false;
+                if (RestoreConnectTimer != null) return;
+                Console.WriteLine(DateTime.Now + " :Пропала связь с " + PrinterName);
+                RestoreConnectTimer = new Timer(RestoreConnect, null, 0, 1000);
+                return;
+            }
+            int iDiStart = 1, iDoStart = 17;
+            int iChTotal;
+            bool[] bDiData, bDoData, bData;
+            //            if (!AdamModbus.Modbus().ReadCoilStatus(iDiStart, m_iDiTotal, out bDiData) ||
+            //                !AdamModbus.Modbus().ReadCoilStatus(iDoStart, m_iDoTotal, out bDoData)) return;
+            if (!AdamModbus.Modbus().ReadCoilStatus(iDiStart, m_iDiTotal, out bDiData) ||
+            !AdamModbus.Modbus().ReadCoilStatus(iDoStart, m_iDoTotal, out bDoData)) return;
+            iChTotal = m_iDiTotal + m_iDoTotal;
+            bData = new bool[iChTotal];
+            if (bDiData == null || bDoData == null) return;
+            Array.Copy(bDiData, 0, bData, 0, m_iDiTotal);
+            Array.Copy(bDoData, 0, bData, m_iDiTotal, m_iDoTotal);
+            InStatus = bData[SignalChannelNumber-1];
+        }
+
+        private void InitializeDevice(DeviceType deviceType, string ipAddress)
+        {
+            //AdamModbus?.Disconnect();
+            AdamModbus = new AdamSocket();
+            AdamModbus.SetTimeout(1000, 1000, 1000); // set timeout for TCP
+            if (AdamModbus.Connect(ipAddress, ProtocolType.Tcp, 502))
+            {
+                if (RestoreConnectTimer == null)
+                    Console.WriteLine(DateTime.Now + "Инициализация прошла успешно: " + PrinterName);
+                IsConnected = true;
+            }
+            else
+            {
+                if (RestoreConnectTimer == null)
+                    Console.WriteLine(DateTime.Now + "Не удалось инициализировать: " + PrinterName);
+                IsConnected = false;
+            }
+            switch (deviceType)
+            {
+                case DeviceType.ADAM6060:
+                    m_iDiTotal = 6;
+                    m_iDoTotal = 6;
+                    break;
+            }
+        }
+
+        public void ReinitializeDevice()
+        {
+            InitializeDevice(DeviceType, IpAddress);
+        }
+
+        private ConcurrentQueue<Guid> FaultIds { get; } = new ConcurrentQueue<Guid>();
+
+        private bool _inStatus = true;
+
+        private bool InStatus
+        {
+            get { return _inStatus; }
+            set
+            {
+                if (_inStatus == value) return;
+                _inStatus = value;
+                if (InStatus) return;
+                var docId = Db.CreateNewPallet(PlaceId);
+                if (docId == null) return;
+                if (ReportManager.PrintReport("Амбалаж", PrinterName, "Pallet", docId, 2)) return;
+                FaultIds.Enqueue((Guid)docId);
+                if (!FaultPrintTaskIsRunning)
+                    Task.Factory.StartNew(PrintFromQueue);
+            }
+        }
+
+        private bool FaultPrintTaskIsRunning { get; set; }
+
+        private void PrintFromQueue()
+        {
+            FaultPrintTaskIsRunning = true;
+            while (FaultIds.Count > 0)
+            {
+                Guid id;
+                if (!FaultIds.TryDequeue(out id)) continue;
+                if (!ReportManager.PrintReport("Амбалаж", PrinterName, "Pallet", id, 2))
+                {
+                    FaultIds.Enqueue(id);
+                }
+            }
+            FaultPrintTaskIsRunning = false;
+        }
+    }
+}
---
+++ GammaPrintService/Settings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GammaPrintService
+{
+	[Serializable]
+	public class Settings
+	{
+		private string printerName = "Zebra";
+		private string pdfPath = "c:\\labels\\label.pdf";
+		private string adamIpAddress;
+		private uint portPrintSignal = 1;
+
+		public string PrinterName
+		{
+			get { return printerName; }
+			set { printerName = value; }
+		}
+
+		public string PdfPath
+		{
+			get { return pdfPath; }
+			set { pdfPath = value; }
+		}
+
+		public string AdamIpAddress
+		{
+			get { return adamIpAddress; }
+			set { adamIpAddress = value; }
+		}
+
+		public uint PortPrintSignal
+		{
+			get { return portPrintSignal; }
+			set { portPrintSignal = value; }
+		}
+	}
+}
